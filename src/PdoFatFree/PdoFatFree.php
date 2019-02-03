c2a0196fee4ea2e0cbfefd0780a5501e58ddd3c8
mohsen <moshen.moradnejadi@gmail.com>
2019-02-03 23:04:12 +0330

    first test version

--- src/PdoFatFree/PdoFatFree.php
+++ src/PdoFatFree/PdoFatFree.php
@@ -1,10 +1,36 @@
 <?php
+
 namespace PdoFatFree;
 
-class PdoFatFree
+use DB\SQL\Mapper;
+
+
+class PdoFatFree extends Mapper
 {
-    public static function test()
+    // Instantiate mapper
+    function __construct(DB\SQL $db)
     {
-        return 'Hello World, Composer!';
+        // This is where the mapper and DB structure synchronization occurs
+        parent::__construct($db, 'vendors');
     }
+
+    // Specialized query
+    function listByCity()
+    {
+        return $this->select('vendorID,name,city', null, array('order' => 'city DESC'));
+        /*
+        We could have done the same thing with plain vanilla SQL:
+        return $this->db->exec(
+            'SELECT vendorID,name,city FROM vendors '.
+            'ORDER BY city DESC;'
+        );
+        */
+    }
+
+    public function test(){
+        echo 'ok google';
+    }
+
+
+
 }
