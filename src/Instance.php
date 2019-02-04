2095f5499fb33a67ef6eb6d7f3b709bc644f9b49
mohsen <moshen.moradnejadi@gmail.com>
2019-02-04 14:33:38 +0330

    so word added

--- src/Instance.php
+++ src/Instance.php
@@ -2,16 +2,16 @@
 
 namespace moradnejadi\pdofatfree;
 
-//use moradnejadi\pdofatfree\DB\SQL\Mapper;
+use moradnejadi\pdofatfree\DB\SQL\Mapper;
 
-class Instance //extends Mapper
+class Instance extends Mapper
 {
     // Instantiate mapper
-    //function __construct(DB\SQL $db)
-    //{
+    function __construct(moradnejadi\pdofatfree\DB\SQL $db)
+    {
         // This is where the mapper and DB structure synchronization occurs
-    //    parent::__construct($db, 'vendors');
-   // }
+        parent::__construct($db, 'vendors');
+    }
 
     // Specialized query
     function listByCity()
